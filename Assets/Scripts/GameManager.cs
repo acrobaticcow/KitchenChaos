8ec7df414db303836a86158681dbc335decb3d58
acrobaticcow <singed8@gmail.com>
2025-07-01 12:04:03 +0700

    add android build setting

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,121 +5,121 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public event EventHandler OnPauseGame;
-    public event EventHandler OnUnPauseGame;
-    public static GameManager Instance;
-    public event EventHandler<OnStateChangedEventArg> OnStateChanged;
-    public event EventHandler OnLocalPlayerReadyChanged;
+	public event EventHandler OnPauseGame;
+	public event EventHandler OnUnPauseGame;
+	public static GameManager Instance;
+	public event EventHandler<OnStateChangedEventArg> OnStateChanged;
+	public event EventHandler OnLocalPlayerReadyChanged;
 
-    public class OnStateChangedEventArg : EventArgs
-    {
-        public State state;
-    }
+	public class OnStateChangedEventArg : EventArgs
+	{
+		public State state;
+	}
 
-    public enum State
-    {
-        WaitingToStart,
-        CountDownToStart,
-        GamePlaying,
-        GameOver
-    }
+	public enum State
+	{
+		WaitingToStart,
+		CountDownToStart,
+		GamePlaying,
+		GameOver
+	}
 
-    private State state;
-    private bool isLocalPlayerReady = false;
-    private float waitingToStartTimer = .5f;
-    private float countDownToStartTimer = 3f;
-    private float gamePlayingTimer;
-    private float gamePlayingTimerMax = 90;
-    private bool isGamePause;
+	private State state;
+	private bool isLocalPlayerReady = false;
+	private float waitingToStartTimer = .5f;
+	private float countDownToStartTimer = 3f;
+	private float gamePlayingTimer;
+	private float gamePlayingTimerMax = 90;
+	private bool isGamePause;
 
-    private void Awake()
-    {
-        state = State.WaitingToStart;
-        Instance = this;
-    }
+	private void Awake()
+	{
+		state = State.WaitingToStart;
+		Instance = this;
+	}
 
-    private void Start()
-    {
-        GameInput.Instance.OnPause += GameInput_OnPause;
-    }
+	private void Start()
+	{
+		GameInput.Instance.OnPause += GameInput_OnPause;
+	}
 
-    private void Update()
-    {
-        switch (state)
-        {
-            case State.WaitingToStart:
-                waitingToStartTimer -= Time.deltaTime;
-                if (waitingToStartTimer < 0f)
-                {
-                    state = State.CountDownToStart;
-                    OnStateChanged?.Invoke(this, new OnStateChangedEventArg { state = state });
-                }
-                break;
-            case State.CountDownToStart:
-                countDownToStartTimer -= Time.deltaTime;
-                if (countDownToStartTimer < 0f)
-                {
-                    state = State.GamePlaying;
-                    OnStateChanged?.Invoke(this, new OnStateChangedEventArg { state = state });
-                    gamePlayingTimer = gamePlayingTimerMax;
-                }
-                break;
-            case State.GamePlaying:
-                gamePlayingTimer -= Time.deltaTime;
-                if (gamePlayingTimer < 0f)
-                {
-                    state = State.GameOver;
-                    OnStateChanged?.Invoke(this, new OnStateChangedEventArg { state = state });
-                }
-                break;
-            case State.GameOver:
-                break;
-        }
-    }
+	private void Update()
+	{
+		switch (state)
+		{
+			case State.WaitingToStart:
+				waitingToStartTimer -= Time.deltaTime;
+				if (waitingToStartTimer < 0f)
+				{
+					state = State.CountDownToStart;
+					OnStateChanged?.Invoke(this, new OnStateChangedEventArg { state = state });
+				}
+				break;
+			case State.CountDownToStart:
+				countDownToStartTimer -= Time.deltaTime;
+				if (countDownToStartTimer < 0f)
+				{
+					state = State.GamePlaying;
+					OnStateChanged?.Invoke(this, new OnStateChangedEventArg { state = state });
+					gamePlayingTimer = gamePlayingTimerMax;
+				}
+				break;
+			case State.GamePlaying:
+				gamePlayingTimer -= Time.deltaTime;
+				if (gamePlayingTimer < 0f)
+				{
+					state = State.GameOver;
+					OnStateChanged?.Invoke(this, new OnStateChangedEventArg { state = state });
+				}
+				break;
+			case State.GameOver:
+				break;
+		}
+	}
 
-    private void GameInput_OnPause(object sender, EventArgs e)
-    {
-        TogglePauseGame();
-    }
+	private void GameInput_OnPause(object sender, EventArgs e)
+	{
+		TogglePauseGame();
+	}
 
-    public bool GetIsLocalPlayerReady()
-    {
-        return isLocalPlayerReady;
-    }
+	public bool GetIsLocalPlayerReady()
+	{
+		return isLocalPlayerReady;
+	}
 
-    public void SetIsLocalPlayerReady(bool value)
-    {
-        isLocalPlayerReady = value;
-        OnLocalPlayerReadyChanged?.Invoke(this, EventArgs.Empty);
-    }
+	public void SetIsLocalPlayerReady(bool value)
+	{
+		isLocalPlayerReady = value;
+		OnLocalPlayerReadyChanged?.Invoke(this, EventArgs.Empty);
+	}
 
-    public void TogglePauseGame()
-    {
-        isGamePause = !isGamePause;
-        if (isGamePause)
-        {
-            Time.timeScale = 0f;
-            OnPauseGame?.Invoke(this, EventArgs.Empty);
-        }
-        else
-        {
-            Time.timeScale = 1f;
-            OnUnPauseGame?.Invoke(this, EventArgs.Empty);
-        }
-    }
+	public void TogglePauseGame()
+	{
+		isGamePause = !isGamePause;
+		if (isGamePause)
+		{
+			Time.timeScale = 0f;
+			OnPauseGame?.Invoke(this, EventArgs.Empty);
+		}
+		else
+		{
+			Time.timeScale = 1f;
+			OnUnPauseGame?.Invoke(this, EventArgs.Empty);
+		}
+	}
 
-    public bool IsGamePlaying()
-    {
-        return state == State.GamePlaying;
-    }
+	public bool IsGamePlaying()
+	{
+		return state == State.GamePlaying;
+	}
 
-    public float GetCountDownToStartTimer()
-    {
-        return countDownToStartTimer;
-    }
+	public float GetCountDownToStartTimer()
+	{
+		return countDownToStartTimer;
+	}
 
-    public float GetGamePlayingTimerNormalized()
-    {
-        return 1 - (gamePlayingTimer / gamePlayingTimerMax);
-    }
+	public float GetGamePlayingTimerNormalized()
+	{
+		return 1 - (gamePlayingTimer / gamePlayingTimerMax);
+	}
 }
--- Assets/Scripts/UI/WaitingForOthersUI.cs
+++ Assets/Scripts/UI/WaitingForOthersUI.cs
@@ -5,28 +5,28 @@ using UnityEngine;
 
 public class WaitingForOthersUI : MonoBehaviour
 {
-    private void Start()
-    {
-        Hide();
-        GameManager.Instance.OnLocalPlayerReadyChanged +=
-            WaitingForOthersUI_OnLocalPlayerReadyChanged;
-    }
+	private void Start()
+	{
+		Show();
+		GameManager.Instance.OnLocalPlayerReadyChanged +=
+			WaitingForOthersUI_OnLocalPlayerReadyChanged;
+	}
 
-    private void WaitingForOthersUI_OnLocalPlayerReadyChanged(object sender, EventArgs e)
-    {
-        if (GameManager.Instance.GetIsLocalPlayerReady())
-        {
-            Show();
-        }
-    }
+	private void WaitingForOthersUI_OnLocalPlayerReadyChanged(object sender, EventArgs e)
+	{
+		if (GameManager.Instance.GetIsLocalPlayerReady())
+		{
+			Hide();
+		}
+	}
 
-    private void Show()
-    {
-        gameObject.SetActive(true);
-    }
+	private void Show()
+	{
+		gameObject.SetActive(true);
+	}
 
-    private void Hide()
-    {
-        gameObject.SetActive(false);
-    }
+	private void Hide()
+	{
+		gameObject.SetActive(false);
+	}
 }
